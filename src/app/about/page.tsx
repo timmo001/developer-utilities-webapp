9aab71d349c85872d48826de3cd4581a097977ec
Aidan Timson <aidan@timmo.dev>
2024-04-27 23:13:24 +0100

    Update About page content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,8 +11,30 @@ export default async function About(): Promise<JSX.Element> {
       <Header />
       <h2 className="text-2xl font-bold">About</h2>
       <p className="mt-4 text-lg text-gray-200">
-        This is a placeholder for the about page.
+        This is a webapp that provides a collection of utilities for developers.
+        It is built using Next.js and TypeScript.
       </p>
+      <p className="mt-4 text-lg text-gray-200">
+        This webapp provides a single place for developers to access a
+        collection of utilities that they might need in their day-to-day work.
+        It also provides an alternative place to use private data that you don't
+        want to share with other services.
+      </p>
+
+      <h3 className="mt-8 text-xl font-bold">Links</h3>
+
+      <ul className="mt-4 text-lg text-gray-200">
+        <li>
+          <a
+            href="https://github.com/timmo001/developer-utilities-webapp"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-400"
+          >
+            GitHub
+          </a>
+        </li>
+      </ul>
     </section>
   );
 }
