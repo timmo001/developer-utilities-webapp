08892e69bdc673b891daebbd40b2dfd4cf824119
Aidan Timson <aidan@timmo.dev>
2024-04-28 00:30:38 +0100

    Add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,32 @@
+import { MetadataRoute } from "next";
+
+import { linksData } from "@/components/shared/links";
+
+const baseUrl = "https://developer-utilities.timmo.dev";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const result: MetadataRoute.Sitemap = [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+    },
+  ];
+
+  for (const link of Object.values(linksData)) {
+    result.push({
+      url: `${baseUrl}${link.href}`,
+      lastModified: new Date(),
+    });
+
+    if (link.paths) {
+      for (const path of link.paths) {
+        result.push({
+          url: `${baseUrl}${path.href}`,
+          lastModified: new Date(),
+        });
+      }
+    }
+  }
+
+  return result;
+}
