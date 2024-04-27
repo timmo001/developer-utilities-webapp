d4cac04959d296f443d322da9b8c2aa6ee03c315
Aidan Timson <aidan@timmo.dev>
2024-04-27 22:20:45 +0100

    Update metadata in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Developer Utilities",
+  applicationName: "Developer Utilities",
   description: "A collection of utilities for developers.",
+  keywords: ["developer", "utilities"],
 };
 
 export default function RootLayout({
