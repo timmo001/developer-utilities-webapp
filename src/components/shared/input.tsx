5a84a8bf0909fc10af5fc3af66307f6425ab095e
Aidan Timson <aidan@timmo.dev>
2024-04-28 10:26:02 +0100

    Add paste button to input

--- src/components/shared/input.tsx
+++ src/components/shared/input.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { mdiContentPaste } from "@mdi/js";
+import Icon from "@mdi/react";
 
 export default function Input({
   multiline,
@@ -9,25 +11,47 @@ export default function Input({
   value: string;
   setValue: (value: string) => void;
 }): JSX.Element {
+  function handlePaste(): void {
+    navigator.clipboard.readText().then((text) => {
+      setValue(text);
+    });
+  }
+
   if (multiline)
     return (
-      <textarea
-        className="mt-4 text-md p-4 w-full text-gray-200 flex items-center rounded transition-colors duration-300 bg-slate-900 hover:bg-slate-800 text-wrap break-words"
+      <div className="relative mt-4 w-full">
+        <textarea
+          className="text-md p-4 w-full text-gray-200 flex items-center rounded transition-colors duration-300 bg-slate-900 text-wrap break-words"
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+          rows={8}
+        />
+        <button
+          className="absolute top-4 right-4 p-2 flex items-center rounded transition-colors duration-300 bg-slate-800 hover:bg-slate-700"
+          onClick={handlePaste}
+        >
+          <Icon title="Paste" size={0.8} path={mdiContentPaste} />
+        </button>
+      </div>
+    );
+
+  return (
+    <div className="relative mt-4 w-full">
+      <input
+        className="text-md p-4 w-full text-gray-200 flex items-center rounded transition-colors duration-300 bg-slate-900"
         value={value}
         onChange={(e) => {
           setValue(e.target.value);
         }}
-        rows={8}
       />
-    );
-
-  return (
-    <input
-      className="mt-4 text-md p-4 w-full text-gray-200 flex items-center rounded transition-colors duration-300 bg-slate-900 hover:bg-slate-800"
-      value={value}
-      onChange={(e) => {
-        setValue(e.target.value);
-      }}
-    />
+      <button
+        className="absolute top-4 right-4 p-2 flex items-center rounded transition-colors duration-300 bg-slate-800 hover:bg-slate-700"
+        onClick={handlePaste}
+      >
+        <Icon title="Paste" size={0.8} path={mdiContentPaste} />
+      </button>
+    </div>
   );
 }
