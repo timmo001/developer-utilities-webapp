8ebb52181e694ee02ef9decb1601a54361001754
Aidan Timson <contact@timmo.xyz>
2024-04-29 09:28:10 +0100

    Fix description

--- src/app/tools/base64-encoder/page.tsx
+++ src/app/tools/base64-encoder/page.tsx
@@ -5,7 +5,7 @@ import Base64Encoder from "@/components/base64-encoder";
 
 export const metadata: Metadata = {
   title: "Base64 Encoder | Developer Utilities",
-  description: "Decode a Base64 encoded string.",
+  description: "Base64 encode a string.",
 };
 
 export default async function Page(): Promise<JSX.Element> {
