e65933af39b798cbe9273fa394bc41745edf417f
Aidan Timson <contact@timmo.xyz>
2024-06-13 12:36:00 +0100

    Add icons to shortcuts

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -14,6 +14,14 @@ import { metadata as xmlFormatterMetadata } from "@/app/tools/xml-formatter/page
 import { metadata as xmlMinifierMetadata } from "@/app/tools/xml-minifier/page";
 
 export default function manifest(): MetadataRoute.Manifest {
+  const icons = [
+    {
+      src: "/icon",
+      sizes: "256x256",
+      type: "image/png",
+    },
+  ];
+
   const manifest: MetadataRoute.Manifest = {
     name: mainMetadata.title as string,
     short_name: mainMetadata.title as string,
@@ -24,68 +32,73 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#020617",
     categories: mainMetadata.keywords as Array<string>,
     lang: "en",
-    icons: [
-      {
-        src: "/icon",
-        sizes: "256x256",
-        type: "image/png",
-      },
-    ],
+    icons,
     shortcuts: [
       {
         name: base64DecoderMetadata.title as string,
         description: base64DecoderMetadata.description as string,
         url: "/tools/base64-decoder",
+        icons,
       },
       {
         name: base64EncoderMetadata.title as string,
         description: base64EncoderMetadata.description as string,
         url: "/tools/base64-encoder",
+        icons,
       },
       {
         name: jsonFormatterMetadata.title as string,
         description: jsonFormatterMetadata.description as string,
         url: "/tools/json-formatter",
+        icons,
       },
       {
         name: jsonMinifierMetadata.title as string,
         description: jsonMinifierMetadata.description as string,
         url: "/tools/json-minifier",
+        icons,
       },
       {
         name: jsonToCSVMetadata.title as string,
         description: jsonToCSVMetadata.description as string,
         url: "/tools/json-to-csv",
+        icons,
       },
       {
         name: jwtDecoderMetadata.title as string,
         description: jwtDecoderMetadata.description as string,
         url: "/tools/jwt-decoder",
+        icons,
       },
       {
         name: urlEncoderMetadata.title as string,
         description: urlEncoderMetadata.description as string,
         url: "/tools/url-encoder",
+        icons,
       },
       {
         name: urlDecoderMetadata.title as string,
         description: urlDecoderMetadata.description as string,
         url: "/tools/url-decoder",
+        icons,
       },
       {
         name: uuidGeneratorMetadata.title as string,
         description: uuidGeneratorMetadata.description as string,
         url: "/tools/uuid-generator",
+        icons,
       },
       {
         name: xmlFormatterMetadata.title as string,
         description: xmlFormatterMetadata.description as string,
         url: "/tools/xml-formatter",
+        icons,
       },
       {
         name: xmlMinifierMetadata.title as string,
         description: xmlMinifierMetadata.description as string,
         url: "/tools/xml-minifier",
+        icons,
       },
     ],
   };
